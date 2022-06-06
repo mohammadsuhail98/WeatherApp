27e2933f0cd55df1e6ea9509e226eb0d6cd3c181
mohammadsuhail98 <msuhayl@earthlinktele.com>
2022-06-06 21:16:10 +0200

    Created Direction enum and LocationHelper class.

---
+++ WeatherApp/Commons/Enums/LocationDirection.swift
@@ -0,0 +1,15 @@
+//
+//  LocationDirection.swift
+//  WeatherApp
+//
+//  Created by mohammad suhail on 6/6/22.
+//
+
+import Foundation
+
+enum LocationDirection: Double {
+    case north = 0
+    case east = 90
+    case south = 180
+    case west = 270
+}
