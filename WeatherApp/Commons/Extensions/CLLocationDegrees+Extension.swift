68d60a7c0b8df3f2c9a035fbd58010c0cb77fd41
mohammadsuhail98 <msuhayl@earthlinktele.com>
2022-06-07 01:56:07 +0200

    Added CLLocationDegrees extension.

---
+++ WeatherApp/Commons/Extensions/CLLocationDegrees+Extension.swift
@@ -0,0 +1,17 @@
+//
+//  CLLocationDegrees+Extension.swift
+//  WeatherApp
+//
+//  Created by mohammad suhail on 7/6/22.
+//
+
+import Foundation
+import MapKit
+
+extension CLLocationDegrees {
+    
+    var toString: String {
+        return "\(self)"
+    }
+    
+}
