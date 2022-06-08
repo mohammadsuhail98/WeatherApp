aec00701aa0e9578f38a70d1df306c415efe4ff5
mohammadsuhail98 <msuhayl@earthlinktele.com>
2022-06-08 22:14:37 +0200

    Added unit tests for fetching weather data.

--- WeatherAppTests/WeatherAppTests.swift
+++ WeatherAppTests/WeatherAppTests.swift
@@ -10,27 +10,56 @@ import XCTest
 
 class WeatherAppTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
+    var weather: Weather?
+    var requestSuccess = false
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
+    func testWeatherInformation(){
+        let pred = NSPredicate(format: "weather != nil")
+        let exp = expectation(for: pred, evaluatedWith: self, handler: nil)
+        WeatherAPIManager.getWeather(route: .fetchWeatherData(lat: "43.94131296851632", lon: "-1.381951168714238", exclude: "daily,minutely,alert", appId: Constants.appID)) { weather in
+            self.weather = weather
+            self.requestSuccess = true
+            exp.fulfill()
+        } errorBlock: { error in
+            self.requestSuccess = false
+        }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
+        let res = XCTWaiter.wait(for: [exp], timeout: 5.0)
 
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+        if res == XCTWaiter.Result.completed {
+            guard let weather = weather else {
+                XCTAssert(false, "weather is nil")
+                return
+            }
+            XCTAssertTrue(requestSuccess)
+            
+            XCTAssertNotNil(weather.current, "Current weather data is nil")
+            XCTAssertNotNil(weather.current?.temp, "The current weather temprature is nil")
+            XCTAssertNotNil(weather.current?.humidity, "The current weather humidity is nil")
+            XCTAssertNotNil(weather.current?.windSpeed, "The current weather wind speed is nil")
+
+        } else {
+            XCTAssert(false, res.debugDescription)
         }
     }
 
+}
+
+extension XCTWaiter.Result: CustomDebugStringConvertible {
+    public var debugDescription: String {
+        switch self {
+        case .completed:
+            return "Completed"
+        case .incorrectOrder:
+            return "Incorrect Order"
+        case .interrupted:
+            return "Interrupted"
+        case .invertedFulfillment:
+            return "Inverted Fulfillment"
+        case .timedOut:
+            return "Timed Out"
+        @unknown default:
+            fatalError("Unknown XCTWaiter.Result case")
+        }
+    }
 }
